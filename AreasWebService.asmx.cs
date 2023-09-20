5626f286d7490f940ab3af624adc3eca5d07086e
Cesar Paolo <galiciacesar21@gmail.com>
2023-09-19 22:03:58 -0600

    Agregar archivos de proyecto.

---
+++ AreasWebService.asmx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AreaWebService
+{
+    /// <summary>
+    /// Descripción breve de AreasWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AreasWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public double AreaCuadrado(double Lado1, double Lado2)
+        {
+            Calcular c = new Calcular();
+            c.numero1 = Lado1;
+            c.numero2 = Lado2;
+
+            return c.AreaCuadrado(Lado1, Lado2);
+        }
+        [WebMethod]
+        public double AreaTriangulo(double Base, double Altura)
+        {
+            Calcular c = new Calcular();
+            c.numero1 = Base;
+            c.numero2 = Altura;
+
+            return c.AreaTriangulo(Base, Altura);
+        }
+        [WebMethod]
+        public double AreaCirculo(double Radio)
+        {
+            Calcular c = new Calcular();
+            c.numero1 = Radio;
+
+            // Calcular el área del círculo (pi * radio * radio)
+            return c.AreaCirculo(Radio);
+        }
+    }
+}
---
+++ Calcular.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AreaWebService
+{
+
+    public class Calcular
+    {
+        public double numero1 { get; set; }
+        public double numero2 { get; set; }
+
+        public double AreaCuadrado(double Lado1 , double Lado2)
+        {
+            numero1 = Lado1;
+            numero2 = Lado2;
+            // Calcular el área del cuadrado (lado * lado)
+            return numero1 * numero2;
+        }
+        public double AreaTriangulo(double Base, double Altura)
+        {
+            var division = 2;
+            numero1 = Base;
+            numero2 = Altura;
+            // Calcular el área del cuadrado (lado * lado)
+            return ((numero1 * numero2)/ division);
+        }
+        public double AreaCirculo(double Radio )
+        {
+            
+            numero1 = Radio;
+            
+            // Calcular el área del círculo (pi * radio * radio)
+            return Math.PI * numero1 * numero1;
+        }
+
+
+
+    }
+
+}
